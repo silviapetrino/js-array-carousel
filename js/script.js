278a1fffded992f0353ca948977111fd50e7547d
silviapetrino <petrino.silvia@gmail.com>
2023-09-13 20:28:01 +0200

    [add] comments

--- js/script.js
+++ js/script.js
@@ -1,35 +1,29 @@
-// 1. prendo i miei elementi per renderli dinamici: immagini e bottoni
-// 2. creo un ciclo che ad ogni interazione mostri un'immagine
-// 3 creo un contatore 
-// 4. salvo le immagini in un array nuovo
-  // perché ad ogni passaggio la classe hide andrà tolta dall'immagine, +inserisco una classe item all'immagine
 
-// 5. aggiungo/tolgo la classe hide al click del bottone 
-// 6. aggiungo/tolgo la classe hide al click del bottone 
-// 7. aggiungo secondo carosello  con due ciclo for
 
+// 1. prendo i miei elementi per renderli dinamici
 
 const containerSlider = document.querySelector(".images");
-
 const secondSlider = document.querySelector(".second-carousel");
 
 
 const images = [
- './img/01.webp',
- "./img/02.webp",
- "./img/03.webp",
- "./img/04.webp",
- "./img/05.webp",
+  './img/01.webp',
+  "./img/02.webp",
+  "./img/03.webp",
+  "./img/04.webp",
+  "./img/05.webp",
 ]
 
 
 const buttonTop = document.querySelector(".button-top");
 const buttonBottom = document.querySelector(".button-bottom");
-
+//  il bottoneTop di default è nascosto
 buttonTop.classList.add("hide");
 
+// 4. creo un contatore 
 let counter = 0;
 
+// 2. creo un ciclo for che ad ogni interazione mostri un'immagine
 
 for(i = 0; i < images.length; i++) {
   let image = images[i];
@@ -40,19 +34,20 @@ for(i = 0; i < images.length; i++) {
 }
 
 
+// 3. salvo le immagini in array aggiungendo nuove classi
+
 let imagesCollection = document.getElementsByClassName("item");
 
  imagesCollection[counter].classList.remove("hide");
 
 
-
 let thumbCollection = document.getElementsByClassName("thumb")
 thumbCollection[counter].classList.add("active");
 
-
+// 5. aggiungo/tolgo la classe hide/active alle immagini al click del bottone sotto
 
 buttonBottom.addEventListener("click", function (){
-
+     
   for(i = 0; i < thumbCollection.length; i++) {
       
     thumbCollection[counter + 1].classList.add("active");
@@ -65,6 +60,7 @@ buttonBottom.addEventListener("click", function (){
 
   imagesCollection[counter].classList.add("hide");
 
+    // incremento il contatore 
   counter++;
 
   thumbCollection[counter].classList.add("active");
@@ -73,10 +69,11 @@ buttonBottom.addEventListener("click", function (){
 
   imagesCollection[counter].classList.remove("hide");
 
-
 }
 
 )
+
+// 6. aggiungo/tolgo la classe hide/active alle immagini al click del bottone sopra
 
 
 buttonTop.addEventListener("click", function (){
@@ -91,7 +88,7 @@ buttonTop.addEventListener("click", function (){
 
 
   imagesCollection[counter].classList.add("hide");
-
+       // decremento il contatore 
   counter--;
   
   if (counter === 0) {
@@ -100,8 +97,6 @@ buttonTop.addEventListener("click", function (){
   }
 
   imagesCollection[counter].classList.remove("hide");
-
-
   
 })
 
