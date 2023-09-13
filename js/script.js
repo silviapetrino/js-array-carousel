241d79209759b1ae5bb05915df04d917a7ae933c
silviapetrino <petrino.silvia@gmail.com>
2023-09-13 15:27:06 +0200

    [edit] js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 // 1. prendo i miei elementi per renderli dinamici: immagini e bottoni
 
 const images = [
-  ".img/01.webp",
-  ".img/02.webp",
-  ".img/03.webp",
-  ".img/04.webp",
-  ".img/05.webp",
-  ".img/06.webp",
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp",
+  "img/06.webp",
 
 ]
 
+const containerSlider = document.querySelector(".container");
 
 const buttonTop = document.querySelector("button-top");
 const buttonBottom = document.querySelector("button-bottom");
@@ -18,7 +19,9 @@ const buttonBottom = document.querySelector("button-bottom");
 
 for(i = 0; i < images.length; i++) {
   let image = images[i];
-  console.log(image);
 
+
+  containerSlider.innerHTML += `<img src="{$(image)}" class="w-100 h-100 object-fit-cover img-fluid hide">`
+  console.log(image);
 
 }
