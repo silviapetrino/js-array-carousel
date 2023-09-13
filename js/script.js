7538511d0d684cce0402872dae0e5babeb2894e9
silviapetrino <petrino.silvia@gmail.com>
2023-09-13 15:15:05 +0200

    [add] js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // 1. prendo i miei elementi per renderli dinamici: immagini e bottoni
+
+const images = [
+  ".img/01.webp",
+  ".img/02.webp",
+  ".img/03.webp",
+  ".img/04.webp",
+  ".img/05.webp",
+  ".img/06.webp",
+
+]
+
+
+const buttonTop = document.querySelector("button-top");
+const buttonBottom = document.querySelector("button-bottom");
+
+// 2. creo un ciclo che ad ogni interazione mostri un'immagine
+
+for(i = 0; i < images.length; i++) {
+  let image = images[i];
+  console.log(image);
+
+
+}
